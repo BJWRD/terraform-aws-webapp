0c47cf142d9b311df80ec0c123f14b02f0023ced
Bradley Whitcomb <bradwhitcomb@Bradleys-MacBook-Pro.local>
2022-09-12 17:01:39 +0100

    Prod including EC2/VPC/Security

---
+++ prod/modules/ec2/main.tf
@@ -0,0 +1,46 @@
+locals {
+  name_suffix = "${var.project_name}-${var.environment}"
+}
+
+locals {
+  required_tags = {
+    project     = var.project_name,
+    environment = var.environment,
+  }
+  tags = merge(var.resource_tags, local.required_tags)
+}
+
+#EC2 Webapp Host
+resource "aws_instance" "web" {
+  count         = var.instance_count
+  ami           = var.ami_id
+  subnet_id     = var.subnet_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [var.vpc_security_group_ids]
+  #key_name - allows private key (.pem file) to be used with ec2 instance
+  key_name = "terraform"
+
+  tags = local.tags
+
+  connection {
+    type  = "ssh"
+    user  =  "ec2-user"
+    #private_key is created within AWS EC2 Console, .pem file placed in same directory as .tf
+    private_key = file("./terraform.pem")
+    #allows connection to newly created ec2 instance
+    host = self.public_ip
+   }
+
+provisioner "remote-exec" {
+  #inline command allows you to enter a list of executable commands within your instance
+  inline = [
+    "sudo yum install yum-utils -y",
+    "sudo yum-config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo",
+    "sudo yum install docker-ce docker-ce-cli containerd.io -y",
+    "sudo systemctl docker.service enable --now",
+    "sudo systemctl containerd.service enable --now",
+    "sudo curl -L https://github.com/docker/compose/releases/tag/v2.10.2/docker-compose-$(uname -s)-$(uname-m) -o /usr/local/bin/docker-compose",
+    "sudo chmod +x /usr/local/bin/docker-compose"
+  ]
+ }
+}
---
+++ prod/modules/security/security.tfvars
@@ -0,0 +1,15 @@
+project_name = [
+  "terraform-aws-webapp"
+]
+
+environment = [
+  "prod"
+]
+
+cidr_blocks = [
+  "0.0.0.0/0"
+]
+
+app_instance_security_group = [
+  "app-instance-security-group"
+]
