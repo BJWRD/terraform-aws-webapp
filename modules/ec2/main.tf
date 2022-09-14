c7c3595e909a7cd35774e3bfc81afe86771eb9e5
Bradley Whitcomb <bradwhitcomb@Bradleys-MacBook-Pro.local>
2022-09-14 17:27:09 +0100

    EC2 remote-exec command update

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -34,11 +34,11 @@ resource "aws_instance" "web" {
 provisioner "remote-exec" {
   #inline command allows you to enter a list of executable commands within your instance
   inline = [
-    "sudo yum install yum-utils -y",
-    "sudo yum-config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo",
-    "sudo yum install docker-ce docker-ce-cli containerd.io -y",
-    "sudo systemctl docker.service enable --now",
-    "sudo systemctl containerd.service enable --now",
+    "sudo yum update -y",
+    "sudo amazon-linux-extras install docker -y",
+    "sudo systemctl enable docker --now",
+    "sudo systemctl enable containerd.service --now",
+    "sudo usermod -a -G docker ec2-user",
     "sudo curl -L https://github.com/docker/compose/releases/tag/v2.10.2/docker-compose-$(uname -s)-$(uname-m) -o /usr/local/bin/docker-compose",
     "sudo chmod +x /usr/local/bin/docker-compose"
   ]
