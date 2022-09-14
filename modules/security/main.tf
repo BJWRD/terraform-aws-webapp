de39fbd372b5e35f498029452af00c1352f6e835
Bradley Whitcomb <bradwhitcomb@Bradleys-MacBook-Pro.local>
2022-09-14 08:41:42 +0100

    CIDR Block update

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -17,14 +17,10 @@ resource "aws_security_group" "app_instance_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-  }
-  ingress {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
+    cidr_blocks = [var.cidr_blocks]
   }
 
   egress {
